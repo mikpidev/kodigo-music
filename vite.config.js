13fde2c068b25bae830a25499fd3e5887750a1fc
Miguel Pineda <realmadrid.mi9u31@gmail.com>
2025-09-03 23:10:37 -0600

    se agrego {} faltante en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-
-
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-        '/api': 'http://localhost:3000' {
-        target: 'https://api.deezer.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/deezer/, '')
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
       }
     }
   }
